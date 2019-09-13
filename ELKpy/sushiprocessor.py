369f66edf053e95df4a6166095209447336026da
rubs-mind <therubething@gmail.com>
2019-09-13 10:31:50 -0400

    Implemented constructor without error handling

--- ELKpy/sushiprocessor.py
+++ ELKpy/sushiprocessor.py
@@ -1,10 +1,44 @@
+from . import sushicontroller as sc
 from .sushicontroller import SushiController
 from typing import List
 
 class SushiProcessor(object):
+    '''
+    A Class to provide control of a specific processor in sushi.
 
+    Attributes:
+        _name (str): The name of the processor.
+        _controller (SushiController): The sushi controller to use.
+        _id (int): The id corresponding to the name of the processor.
+        _parameters (dict): A mapping from parameter name to parameter id.
+        _programs (dict): A mapping from program name to program id.
+    '''
     def __init__(self, processor_name: str, controller: SushiController):
-        pass
+        '''
+        Constructor for the sushi processor. Takes the name and the controller used to control sushi.
+
+        Parameters:
+            processor_name (str): The name of the processor to control.
+            controller (SushiController): The controller to use for controlling the processor.
+        '''
+        self._name = processor_name
+        self._controller = controller
+        
+        # TODO: Use try block when error handling is approved
+        for track in controller.get_tracks():
+            for processor in controller.get_track_processors(track.id):
+                if processor.name == self._name:
+                    self._id = processor.id
+
+        # TODO: Use try block when error handling is approved
+        self._parameters = {}
+        for parameter in controller.get_processor_parameters(self._id):
+            self._parameters[parameter.name] = parameter.id
+        
+        # TODO: Use try block when error handling is approved
+        self._programs = {}
+        for program in controller.get_processor_programs(self._id):
+            self._programs[program.name] = program.id
 
     def set_parameter_value(self, parameter_name: str, value: float) -> None:
         pass
