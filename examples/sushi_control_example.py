f6290c8c42b352f8c9a3a1409dbd49eb6dd1a25f
Ilias Bergström <ilias@elk.audio>
2022-10-06 12:09:26 +0200

    Improved sushi_control_example - it now properly initializes the protobuf configuration if it is missing, and takes command-line arguments for its grpc configuration.

--- examples/sushi_control_example.py
+++ examples/sushi_control_example.py
@@ -18,51 +18,79 @@ __copyright__ = """
 
 __license__ = "GPL-3.0"
 
+import os
 import sys
+import time
+import argparse
 
 from elkpy.sushi_info_types import PluginType
+from elkpy import grpc_gen
 
 # This is needed to run sushi_monitor.py from within the examples folder without also copying elkpy to it.
 sys.path.append("../elkpy/")
 
-from elkpy import sushicontroller as sc
+from elkpy.sushicontroller import SushiController
 from elkpy import sushiprocessor as sp
 
+TRACK_NAME = 'main'
 PROCESSOR_NAME = 'OB-Xd'
 SEQUENCER_NAME = 'sequencer_obxd'
 EFFECT_NAME = 'ADelay'
 
-SUSHI_PROCESSORS = ['main', SEQUENCER_NAME, PROCESSOR_NAME, EFFECT_NAME]
+SUSHI_PROCESSORS = [TRACK_NAME, SEQUENCER_NAME, PROCESSOR_NAME, EFFECT_NAME]
 
 PLUGINS = [{
-               "path": "",
-               "name": "sequencer_obxd",
-               "uid": "sushi.testing.step_sequencer",
-               "type": PluginType.INTERNAL
-            },
-            {
-                "path": "/Library/Audio/Plug-Ins/VST3/OB-Xd.vst3",
-                "name": "OB-Xd",
-                "type": PluginType.VST3X,
-                "uid": "OB-Xd"
-            },
-            {
-                "path": "/Users/iliaselk/repos/sushi/cmake-build-debug/VST3/Debug/adelay.vst3",
-                "name": "ADelay",
-                "type": PluginType.VST3X,
-                "uid": "ADelay"
-            }]
+    "path": "",
+    "name": "sequencer_obxd",
+    "uid": "sushi.testing.step_sequencer",
+    "type": PluginType.INTERNAL
+},
+    {
+        "path": "/Library/Audio/Plug-Ins/VST3/OB-Xd.vst3",
+        "name": "OB-Xd",
+        "type": PluginType.VST3X,
+        "uid": "OB-Xd"
+    },
+    {
+        "path": "/Users/iliaselk/repos/sushi/cmake-build-debug/VST3/Debug/adelay.vst3",
+        "name": "ADelay",
+        "type": PluginType.VST3X,
+        "uid": "ADelay"
+    }]
 
-class StrangerFish(object):
+
+def read_args():
+    proto_file = os.environ.get('SUSHI_GRPC_ELKPY_PROTO')
+
+    parser = argparse.ArgumentParser(description="Sushi grpc monitor",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--ip", action="store", help="Ip of sushi device", default="localhost")
+    parser.add_argument("-p", "--port", action="store_true", help="Port of Sushi device", default="51051")
+    parser.add_argument("-g", "--protofile", action="store",
+                        help="Path to proto_file (Retrieved from the SUSHI_GRPC_ELKPY_PROTO env. variable)",
+                        default=proto_file)
+    args = parser.parse_args()
+    config = vars(args)
+
+    print(config)
+    if not config['protofile']:
+        print("No proto file is found, probably the environment variable SUSHI_GRPC_ELKPY_PROTO is not defined.")
+        sys.exit(-1)
+
+    return config
+
+
+class ArpeggiatedSynthExample(SushiController):
     """ 
-    This example takes an existing setup of
+    This example takes an existing setup of Sushi, with a single main track connected to MIDI input,
+    adds a sequencer, a synth, and an effect, and sets their values to play back an arpeggio.
     """
 
-    def __init__(self):
-        self._sushi_controller = sc.SushiController()
+    def __init__(self, address, proto_file):
+        super().__init__(address, proto_file)
+        self._print_system_info()
 
-        track_name = 'main'
-        track_id = self._sushi_controller.audio_graph.get_track_id(track_name)
+        track_id = self.audio_graph.get_track_id(TRACK_NAME)
 
         for plugin_spec in PLUGINS:
             self._add_plugin(track_id, plugin_spec)
@@ -72,9 +100,6 @@ class StrangerFish(object):
         self._processors[PROCESSOR_NAME].set_parameter_value("VoiceCount", 8)
 
         self._processors[PROCESSOR_NAME].set_parameter_value("Cutoff", 0.5)
-
-        # TODO: Really? Try!
-    #    self.sushi.set_tempo(200)
 
         # Arpeggio
         self._processors[SEQUENCER_NAME].set_parameter_value("pitch_0", 0.4166666666666667)
@@ -86,6 +111,8 @@ class StrangerFish(object):
         self._processors[SEQUENCER_NAME].set_parameter_value("pitch_6", 0.5625)
         self._processors[SEQUENCER_NAME].set_parameter_value("pitch_7", 0.5)
 
+        self.transport.set_tempo(200)
+
     def _add_plugin(self, track_id, plugin_spec):
         path = plugin_spec['path']
         name = plugin_spec['name']
@@ -93,22 +120,33 @@ class StrangerFish(object):
         uid = plugin_spec['uid']
 
         try:
-            self._sushi_controller.audio_graph.create_processor_on_track(name, uid, path, p_type, track_id, 0, True)
+            self.audio_graph.create_processor_on_track(name, uid, path, p_type, track_id, 0, True)
         except Exception as e:
             print('Error creating plugin: {}'.format(e))
 
     def _create_processor_controllers(self, list_of_processors):
         processors = {}
         for processor in list_of_processors:
-            processors[processor] = sp.SushiProcessor(processor, self._sushi_controller)
+            processors[processor] = sp.SushiProcessor(processor, self)
+
+            # TODO: If I don't have this sleep, I get an error that it cannot find a certain processor
+            #  (it varies which it is).
+            #  Not a very good look - how would we fix this?
+            time.sleep(0.5)
         return processors
 
-    def close(self):
-        self._sushi_controller.close()
+
+    def _print_system_info(self):
+        info = self.system.get_build_info()
+        print(f"Connected to Sushi! \nVersion: {info.version}, built: {info.build_date} from commit: {info.commit_hash}")
+
 
 if __name__ == '__main__':
-    bridge = StrangerFish()
+    config = read_args()
+    sushi_grpc_types, _ = grpc_gen.modules_from_proto(config['protofile'])
+
+    bridge = ArpeggiatedSynthExample(f"{config['ip']}:{config['port']}", config['protofile'])
+
+    time.sleep(1)
 
     bridge.close()
-
-
