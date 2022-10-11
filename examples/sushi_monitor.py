4f0042c65f184d5193463d948e3148b70155700c
Ilias Bergström <ilias@elk.audio>
2022-10-11 12:09:09 +0200

    Applied some reformatting suggestions from PyCharm to sushi_monitor.py

--- examples/sushi_monitor.py
+++ examples/sushi_monitor.py
@@ -22,7 +22,7 @@ __license__ = "GPL-3.0"
     An example passive monitor app using elkpy.
     It connects to a sushi instance, subscribes to notifications and displays all the
     parameter, transport and audio graph changes that Sushi broadcasts.
-""" 
+"""
 
 import os
 import sys
@@ -38,14 +38,17 @@ from elkpy import grpc_gen
 SYNC_MODES = ['Dummy', 'Internal', 'Midi', 'Link']
 PLAYING_MODES = ['Dummy', 'Stopped', 'Playing']
 
+
 def read_args():
     proto_file = os.environ.get('SUSHI_GRPC_ELKPY_PROTO')
-    
+
     parser = argparse.ArgumentParser(description="Sushi grpc monitor",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--ip", action="store", help="Ip of sushi device", default="localhost")
     parser.add_argument("-p", "--port", action="store_true", help="Port of Sushi device", default="51051")
-    parser.add_argument("-g", "--protofile", action="store", help="Path to proto_file (Retrieved from the SUSHI_GRPC_ELKPY_PROTO env. variable)", default=proto_file)
+    parser.add_argument("-g", "--protofile", action="store",
+                        help="Path to proto_file (Retrieved from the SUSHI_GRPC_ELKPY_PROTO env. variable)",
+                        default=proto_file)
     args = parser.parse_args()
     config = vars(args)
 
@@ -57,9 +60,28 @@ def read_args():
     return config
 
 
+def _process_transport_notification(notif):
+    try:
+        if notif.HasField('tempo'):
+            print(f"Tempo set to {notif.tempo} bpm")
+
+        elif notif.HasField('playing_mode'):
+            print(f"Playing mode set to {PLAYING_MODES[notif.playing_mode.mode]}")
+
+        elif notif.HasField('sync_mode'):
+            print(f"tempo sync mode set to {SYNC_MODES[notif.sync_mode.mode]}")
+
+        elif notif.HasField('time_signature'):
+            print(
+                f"Time time_signature set to {notif.time_signature.denominator} / {notif.time_signature.numerator}")
+
+    except Exception as e:
+        print(e)
+
+
 class Listener(SushiController):
 
-    def __init__(self, address, proto_file):
+    def __init__(self, address: str, proto_file: str):
         super().__init__(address, proto_file)
         self._print_system_info()
         self._read_audio_graph()
@@ -68,11 +90,12 @@ class Listener(SushiController):
         self.notifications.subscribe_to_processor_changes(self._process_processor_notification)
         self.notifications.subscribe_to_parameter_updates(self._process_parameter_notification)
         self.notifications.subscribe_to_property_updates(self._process_property_notification)
-        self.notifications.subscribe_to_transport_changes(self._process_transport_notification)
+        self.notifications.subscribe_to_transport_changes(_process_transport_notification)
 
     def _print_system_info(self):
         info = self.system.get_build_info()
-        print(f"Connected to Sushi! \nVersion: {info.version}, built: {info.build_date} from commit: {info.commit_hash}")
+        print(f"Connected to Sushi! \n"
+              f"Version: {info.version}, built: {info.build_date} from commit: {info.commit_hash}")
 
     def _read_audio_graph(self):
         self._processors = {}
@@ -84,17 +107,16 @@ class Listener(SushiController):
         print(f"A total of {len(processors)} processors on {len(tracks)} tracks")
 
     def _add_processor(self, info):
-        processor = {}
-        processor['info'] = info
-        processor['parameters'] = {param.id : param for param in self.parameters.get_processor_parameters(info.id)}
-        processor['properties'] = {prop.id : prop for prop in self.parameters.get_processor_properties(info.id)}
+        processor = {'info': info,
+                     'parameters': {param.id: param for param in self.parameters.get_processor_parameters(info.id)},
+                     'properties': {prop.id: prop for prop in self.parameters.get_processor_properties(info.id)}}
         self._processors[info.id] = processor
 
     def _process_track_notification(self, notif):
-        if notif.action == 1:   # TRACK_ADDED
+        if notif.action == 1:  # TRACK_ADDED
             info = self.audio_graph.get_processor_info(notif.track.id)
             self._add_processor(info)
-            print(f"Track \"{info.name}\" ({info.id}) added") 
+            print(f"Track \"{info.name}\" ({info.id}) added")
 
         elif notif.action == 2:  # TRACK_DELETED
             info = self._processors[notif.track.id]['info']
@@ -103,9 +125,9 @@ class Listener(SushiController):
     def _process_processor_notification(self, notif):
         track = self._processors[notif.parent_track.id]['info']
 
-        if notif.action == 1:   # PROCESSOR_ADDED
+        if notif.action == 1:  # PROCESSOR_ADDED
             self._add_processor(info)
-            print(f"Processor \"{info.name}\" ({info.id}) added to track \"{track.name}\" ({track.id})") 
+            print(f"Processor \"{info.name}\" ({info.id}) added to track \"{track.name}\" ({track.id})")
 
         elif notif.action == 2:  # PROCESSOR_DELETED
             info = self._processors[notif.processor.id]['info']
@@ -115,7 +137,8 @@ class Listener(SushiController):
         try:
             processor = self._processors[notif.parameter.processor_id]
             info = processor['parameters'][notif.parameter.parameter_id]
-            print(f"Parameter {info.name} on {processor['info'].name} set to {notif.formatted_value} {info.unit.strip()} ({notif.normalized_value:.3f})")
+            print(f"Parameter {info.name} on {processor['info'].name} "
+                  f"set to {notif.formatted_value} {info.unit.strip()} ({notif.normalized_value:.3f})")
 
         except Exception as e:
             print(e)
@@ -125,26 +148,10 @@ class Listener(SushiController):
             processor = self._processors[notif.property.processor_id]
             info = processor['properties'][notif.property.property_id]
             print(f"Property {info.name} on {processor['info'].name} set to {notif.value}")
-        
-        except Exception as e:
-            print(e)
-
-    def _process_transport_notification(self, notif):
-        try:
-            if notif.HasField('tempo'):
-                print(f"Tempo set to {notif.tempo} bpm")
-
-            elif notif.HasField('playing_mode'):
-                print(f"Playing mode set to {PLAYING_MODES[notif.playing_mode.mode]}")
-
-            elif notif.HasField('sync_mode'):
-                print(f"tempo sync mode set to {SYNC_MODES[notif.sync_mode.mode]}")
-
-            elif notif.HasField('time_signature'):
-                print(f"Time time_signature set to {notif.time_signature.denominator} / {notif.time_signature.numerator}")
 
         except Exception as e:
             print(e)
+
 
 def main():
     config = read_args()
@@ -157,10 +164,12 @@ def main():
             time.sleep(1)
 
         except KeyboardInterrupt:
-            listener.close() # calls the close() method inherited from SushiController
+            # The close() method is inherited from SushiController
+            listener.close()
             sys.exit(0)
 
     listener.close()
+
 
 if __name__ == '__main__':
     main()
