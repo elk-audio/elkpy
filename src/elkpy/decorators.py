dea8c3fb7f8940483b03decca5863323fd4757db
max <max@elk.audio>
2024-02-22 14:50:18 +0100

    added add_wait_event decorator

---
+++ src/elkpy/decorators.py
@@ -0,0 +1,17 @@
+from typing_extensions import Callable
+import asyncio
+
+
+def add_wait_event(wait_manager):
+    """This will add an asyncio.Event to the passed wait_manager and also add it as a return value to the decorated function."""
+    def add_event(fun: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            fun(*args, **kwargs)
+            e = asyncio.Event()
+            wait_manager.event_list.append(e)
+            return e
+        return wrapper
+    return add_event
+
+
+
