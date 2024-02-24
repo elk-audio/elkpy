97e7e78366c4b4a5cff31132050368c70f946d60
max <max@elk.audio>
2024-02-24 16:35:48 +0100

    removed unused code

--- src/elkpy/events.py
+++ src/elkpy/events.py
@@ -1,20 +1,4 @@
-from typing import Callable, TYPE_CHECKING
 import asyncio
-
-if TYPE_CHECKING:
-    from elkpy import sushicontroller
-
-
-def add_wait_event(wait_manager):
-    """This will add an asyncio.Event to the passed wait_manager and also add it as a return value to the decorated function."""
-    def add_event(fun: Callable) -> Callable:
-        def wrapper(*args, **kwargs):
-            fun(*args, **kwargs)
-            e = asyncio.Event()
-            wait_manager.event_list.append(e)
-            return e
-        return wrapper
-    return add_event
 
 
 class ElkpyEvent(asyncio.Event):
